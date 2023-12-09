c4535115eb8461e9ac10c67859bfcf77f7b07c1f
xinj10 <xinj10@outlook.com>
2023-12-09 17:14:50 +0800

    feat: finish playround

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
+const choices = ["Rock", "Paper", "Scissors"];
 const getComputerChoice = () => {
-  const choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * 3)];
 };
+
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection =
+    playerSelection[0].toUpperCase() +
+    playerSelection.substring(1).toLowerCase();
+  const playerChoice = choices.findIndex((c) => c === playerSelection);
+  const computerChoice = choices.findIndex((c) => c === computerSelection);
+  if (playerChoice === 0 && computerChoice === 2) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerChoice < computerChoice) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (playerChoice > computerChoice) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return playRound(playerSelection, getComputerChoice());
+  }
+};
+
+/* for test */
+// console.log(playRound("Rock", "Scissors"));
+// console.log(playRound("Rock", "Paper"));
+// console.log(playRound("Paper", "Scissors"));
+// console.log(playRound("Scissors", "Scissors"));
+
